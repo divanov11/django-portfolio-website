5d7e06bdfe54e9146c83d9bbf3b836dbc39e0728
jewells07 <jewellsjoshi437@gmail.com>
2020-12-11 12:40:06 +0530

    Solved error (clear thumbnail)

--- base/models.py
+++ base/models.py
@@ -34,7 +34,7 @@ class Tag(models.Model):
 class Post(models.Model):
 	headline = models.CharField(max_length=200)
 	sub_headline = models.CharField(max_length=200, null=True, blank=True)
-	thumbnail = models.ImageField(null=True, blank=True, upload_to="images", default="/images/placeholder.png")
+	thumbnail = models.ImageField(null=True, blank=True, upload_to="images", default="/placeholder.png")
 	body = RichTextUploadingField(null=True, blank=True)
 	created = models.DateTimeField(auto_now_add=True)
 	active = models.BooleanField(default=False)
